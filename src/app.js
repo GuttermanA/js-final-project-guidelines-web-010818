020ffea1aadc1709804c42a955f4d42060116748
GuttermanA <alexanderf.gutterman@gmail.com>
2018-03-06 15:07:42 -0500

    words display in boxes

--- src/app.js
+++ src/app.js
@@ -3,12 +3,19 @@ class App {
     this.rows = document.querySelectorAll('.row')
     this.startGameButton = document.querySelector('#start-game-button')
     //ROWS
-    this.rowOne = document.querySelector('#row-1')
-    this.rowTwo = document.querySelector('#row-2')
-    this.rowThree = document.querySelector('#row-3')
-    this.rowFour = document.querySelector('#row-4')
+    this.rowOne = document.querySelector('#row1')
+    this.rowTwo = document.querySelector('#row2')
+    this.rowThree = document.querySelector('#row3')
+    this.rowFour = document.querySelector('#row4')
     this.addEventListeners()
     this.boxId = 0;
+  }
+
+  addSelectors() {
+    this.rowOneCardBlock = document.querySelectorAll('.row-1')
+    this.rowTwoCardBlock = document.querySelectorAll('.row-2')
+    this.rowThreeCardBlock = document.querySelectorAll('.row-3')
+    this.rowFourCardBlock = document.querySelectorAll('.row-4')
   }
 
   addEventListeners(){
@@ -22,24 +29,66 @@ class App {
   }
 
   fetchPhrase() {
+
     fetch(`http://localhost:3000/phrases`)
       .then(res => res.json())
-      .then(json => console.log(json))
+      .then(json => {
+        // this.boxId = 0;
+        // this.generateBoxes();
+        let phrase = new Phrase(json)
+        console.log(phrase)
+        console.log(this)
+        this.renderPhrase(phrase.process())
+      })
   }
 
-  renderBox() {
-    return  `
+  renderPhrase(displayObject) {
+    for(let k in displayObject) {
+      if (displayObject[k].length >= 0) {
+        if (k === this.rowOne.id) {
+          for(let i = 0; i < displayObject[k].length; i++){
+            this.rowOneCardBlock[i].innerText = displayObject[k][i]
+            console.log(this.rowOneCardBlock[i].innerText);
+          }
+        }
+        if (k === this.rowTwo.id) {
+          for(let i = 0; i < displayObject[k].length; i++){
+            this.rowTwoCardBlock[i].innerText = displayObject[k][i]
+          }
+        }
+        if (k === this.rowThree.id) {
+          for(let i = 0; i < displayObject[k].length; i++){
+            this.rowThreeCardBlock[i].innerText = displayObject[k][i]
+          }
+        }
+        if (k === this.rowFour.id) {
+          for(let i = 0; i < displayObject[k].length; i++){
+            this.rowFourCardBlock[i].innerText = displayObject[k][i]
+          }
+        }
+      }
+    }
+  }
+
+  renderBox(cardBlockText="") {
+    return `
     <div class="col-lg-1 ">
-      <div class="card bg-success empty-box mb-3" id="box-${++this.boxId}">
-        <div class="card-block">
+      <div class="card bg-success empty-box mb-3">
+       <div class="card-block row-${Math.floor(this.boxId++/12) + 1}">
+       ${cardBlockText}
        </div>
       </div>
     </div>
     `
   }
 
+  renderConsenantButtons() {
+    
+  }
+
   generateBoxes(){
     for (let row of this.rows) {
+      row.innerHTML = ""
       for(let i = 0; i < 12; i++) {
         row.innerHTML += this.renderBox()
       }
--- src/phrase.js
+++ src/phrase.js
@@ -1,8 +1,72 @@
 class Phrase {
-  constructor({ text, category_id}) {
+  constructor({ text, category_name}) {
     this.text = text;
-    this.categoryId = category_id
+    this.categoryName = category_name
+    this.process = this.process.bind(this)
   }
 
-  
+process() {
+  let displayObject = {row1: [], row2:[], row3:[], row4:[]};
+  let splitPhrase = this.text.split(/(\s+)/);
+
+  let rowNum = 1;
+  let currentRowRemainder = 12;
+
+  for (let i = 0; i < splitPhrase.length; i++) {
+    if (currentRowRemainder - splitPhrase[i].length >= 0) {
+      displayObject[`row${rowNum}`].push(splitPhrase[i]);
+      currentRowRemainder -= splitPhrase[i].length;
+    } else {
+      rowNum++;
+      currentRowRemainder = 12;
+      displayObject[`row${rowNum}`].push(splitPhrase[i]);
+    }
+  }
+  for(let k in displayObject) {
+    displayObject[k] = displayObject[k].join("").split("");
+  }
+  console.log(displayObject);
+  return displayObject;
+}
+
+
+  //ORIGINAL PROCESS
+  // process() {
+  //   let displayObject = {row1: [], row2:[], row3:[], row4:[]}
+  //   let splitPhrase = this.text.split(/(\s+)/)
+  //   debugger
+  //   for (let i = 0; i < splitPhrase.length; i++) {
+  //     if ((displayObject["row1"].join(" ").length + splitPhrase[i].length) <= 12) {
+  //       displayObject["row1"].push(splitPhrase[i])
+  //     } else if ((displayObject["row2"].join(" ").length + splitPhrase[i].length) <= 12) {
+  //       displayObject["row2"].push(splitPhrase[i])
+  //     } else if ((displayObject["row3"].join(" ").length + splitPhrase[i].length) <= 12) {
+  //       displayObject["row3"].push(splitPhrase[i])
+  //     } else if ((displayObject["row4"].join(" ").length + splitPhrase[i].length) <= 12) {
+  //       displayObject["row4"].push(splitPhrase[i])
+  //     } else {
+  //       console.error("Phrase is too long to fit")
+  //     }
+  //   }
+  //   for(let k in displayObject) {
+  //     displayObject[k] = displayObject[k].join("").split("")
+  //   }
+  //   console.log(displayObject);
+  //   return displayObject
+  // }
+
+
+  //PROCESS WITH WHILE
+  // process() {
+  //   let splitPhrase = this.text.split(/(\s+)/)
+  //   for(let k in this.displayObject) {
+  //     let rowLength = this.
+  //     while (this.displayObject[k].length <= 12) {
+  //       for(let word of splitPhrase) {
+  //         this.displayObject[k].push(word)
+  //       }
+  //     }
+  //   }
+  // }
+
 }
